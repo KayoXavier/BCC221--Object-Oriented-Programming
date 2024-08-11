a83e86ec18ebb897cc5adabfb1965f19525fea37
KayoXavier <106191487+KayoXavier@users.noreply.github.com>
2024-08-11 11:44:55 -0300

    Create mecanico.h

---
+++ TP1/Include/mecanico.h
@@ -0,0 +1,20 @@
+#ifndef MECANICO_H
+#define MECANICO_H
+
+#include "Funcionario.h"
+
+using namespace std;
+
+class Mecanico : public Funcionario {
+public:
+    // Construtor de Mecanico
+    Mecanico(const string& nome, const string& cpf, int idFuncionario);
+
+    // Implementação do método realizarTarefas
+    void realizarTarefas() const override;
+
+    // Outros métodos específicos do Mecanico
+    void executarManutencao();  // Exemplo de método específico
+};
+
+#endif
